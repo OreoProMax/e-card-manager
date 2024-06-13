05722800b8b9afd24980f30254388ec5245f028d
OreoProMax <OuHY11228@gmail.com>
2024-06-13 10:47:23 +0800

    实现E卡管理器元素挂载

---
+++ src/components/CardManager.vue
@@ -0,0 +1,9 @@
+<template></template>
+
+<script setup lang="ts">
+import { onMounted } from "vue";
+
+onMounted(() => {
+  console.log("CardManager mounted");
+});
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,28 @@
-import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-import ElementPlus from 'element-plus'
-import 'element-plus/theme-chalk/index.css'
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import ElementPlus from "element-plus";
+import "element-plus/theme-chalk/index.css";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
 
+import { ElMessage } from "element-plus";
 createApp(App)
   .use(ElementPlus, {
     locale: zhCn,
   })
   .mount(
   (() => {
-    const app = document.createElement('div');
-    document.body.append(app);
+    // 选择挂载位置
+    const mountLocation = document.querySelector("div.giftcard-tab.ml20");
+    // 创建一个div元素
+    const app = document.createElement("div");
+    // 将创建的div元素添加到选择的元素中
+    if (mountLocation) {
+      mountLocation.appendChild(app);
+    } else {
+      ElMessage.error("京东E卡管理器：挂载位置不存在，脚本可能已经失效！")
+    }
     return app;
   })(),
 );

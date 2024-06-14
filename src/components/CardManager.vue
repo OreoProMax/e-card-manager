0c8994d329b28707a3e8ce9a2f0abe1e0eb638cb
OreoProMax <OuHY11228@gmail.com>
2024-06-15 00:21:25 +0800

    实现选择/反选E卡的主要功能

--- src/components/CardManager.vue
+++ src/components/CardManager.vue
@@ -8,24 +8,60 @@
     E卡管理
   </el-button>
 
-  <el-dialog :title="scriptTitle" v-model="dialogVisible" @open="countCards">
+  <el-dialog
+    v-model="dialogVisible"
+    :title="scriptTitle"
+    width="60%"
+    @open="countCards"
+  >
     <el-table
       :data="tableData"
       show-summary
       :summary-method="getSummary"
       stripe
+      v-loading="loading && { text: loadingText }"
+      :element-loading-text="loadingText"
     >
-      <el-table-column type="index" min-width="8%" />
-      <el-table-column label="余额" prop="balance" min-width="23%" />
-      <el-table-column label="总张数" prop="totalCount" min-width="23%" />
-      <el-table-column label="已选" prop="selectedCount" min-width="23%" />
-      <el-table-column label="未选" prop="notSelectedCount" min-width="23%" />
+      <el-table-column type="index" min-width="7%" />
+      <el-table-column label="余额" prop="balance" min-width="15%" />
+      <el-table-column label="总张数" prop="totalCount" min-width="15%" />
+      <el-table-column label="已选" prop="selectedCount" min-width="15%" />
+      <el-table-column label="未选" prop="notSelectedCount" min-width="15%" />
+      <el-table-column label="操作" min-width="33%">
+        <template #default="scope">
+          <el-input-number
+            v-model="scope.row.operationNum"
+            :min="0"
+            :max="scope.row.totalCount"
+            size="small"
+            style="width: 40%"
+          />
+
+          张
+
+          <el-button
+            size="small"
+            type="success"
+            @click="handleOperation(scope.row, 1)"
+          >
+            选择
+          </el-button>
+
+          <el-button
+            size="small"
+            type="danger"
+            @click="handleOperation(scope.row, 0)"
+          >
+            反选
+          </el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </el-dialog>
 </template>
 
 <script setup lang="ts">
-import { ElNotification } from "element-plus";
+import { ElMessage, ElNotification } from "element-plus";
 import type { TableColumnCtx } from "element-plus";
 import { onMounted, reactive, ref } from "vue";
 
@@ -33,41 +69,43 @@ const scriptTitle = __SCRIPT_TITLE__;
 
 const dialogVisible = ref(false);
 
-// 创建E卡对象数组，每条记录包含字段有：id、balance、status
-interface ECardObject {
+// E卡对象，字段：element（表示该E卡的HTML元素）、id、balance、status
+interface Card {
+  element: HTMLElement;
   id: string;
   balance: number;
   status: number;
 }
-const eCards = reactive([] as ECardObject[]);
-// 创建E卡表格数组，每条记录包含字段有：balance、totalCount、selectedCount、notSelectedCount
-interface ECardTableEntry {
-  balance: number;
-  totalCount: number;
-  selectedCount: number;
-  notSelectedCount: number;
-}
-const tableData = reactive([] as ECardTableEntry[]);
+const cards = reactive([] as Card[]);
 
+/**
+ * 提取页面上所有E卡元素，存入cards数组中
+ */
 function countCards(): void {
   // 选取页面上所有E卡元素，为class属性包含"g-detail"的div元素
-  const cards = document.querySelectorAll('div[class~="g-detail"]');
+  const cardElements = document.querySelectorAll('div[class~="g-detail"]');
+
   // 遍历每个E卡元素，提取E卡的ID、余额、状态
-  eCards.length = 0; // 清空eCards数组原有内容
-  cards.forEach((card) => {
+  cards.length = 0; // 清空cards数组原有内容
+  cardElements.forEach((cardElement) => {
     // 提取E卡的ID，为E卡元素的data-gift-id属性的值
-    const id = card.getAttribute("data-gift-id");
+    const id = cardElement.getAttribute("data-gift-id");
 
     // 提取E卡的余额，为E卡元素中【class="g-price"】的div元素中的strong子代元素的文本内容
-    const balanceElement = card.querySelector("div.g-price>strong");
+    const balanceElement = cardElement.querySelector("div.g-price>strong");
     const balance = extractNumber(balanceElement);
 
     // 提取E卡的状态，如果E卡元素的class属性包含"item-selected"，则状态为0，否则为1
-    const status = card.className.includes("item-selected") ? 0 : 1;
+    const status = cardElement.className.includes("item-selected") ? 0 : 1;
 
-    // 将E卡的信息存入eCards数组中
-    if (id !== null && balance !== -1) {
-      eCards.push({ id, balance, status });
+    // 将E卡的信息存入cards数组中
+    if (cardElement !== null && id !== null && balance !== -1) {
+      cards.push({
+        element: cardElement as HTMLElement,
+        id: id,
+        balance: balance,
+        status: status,
+      });
     } else {
       ElNotification({
         type: "error",
@@ -77,9 +115,25 @@ function countCards(): void {
     }
   });
 
-  // 计算eCards中的数据，转换到tableData中
+  countTableData();
+}
+
+// E卡表格条目对象，字段：balance、totalCount、selectedCount、notSelectedCount、operationNum（表示本次操作的数量）
+interface CardTableEntry {
+  balance: number;
+  totalCount: number;
+  selectedCount: number;
+  notSelectedCount: number;
+  operationNum: number;
+}
+const tableData = reactive([] as CardTableEntry[]);
+
+/**
+ * 计算cards数组中的数据，转换到tableData中
+ */
+function countTableData(): void {
   tableData.length = 0; // 清空tableData数组原有内容
-  eCards.forEach((card) => {
+  cards.forEach((card) => {
     const { balance, status } = card;
     const index = tableData.findIndex((item) => item.balance === balance);
     if (index === -1) {
@@ -88,6 +142,7 @@ function countCards(): void {
         totalCount: 1,
         selectedCount: status === 0 ? 1 : 0,
         notSelectedCount: status === 1 ? 1 : 0,
+        operationNum: 0,
       });
     } else {
       tableData[index].totalCount += 1;
@@ -99,18 +154,27 @@ function countCards(): void {
   tableData.sort((a, b) => a.balance - b.balance);
 }
 
-interface SummaryMethodProps<T = ECardTableEntry> {
+interface SummaryMethodProps<T = CardTableEntry> {
   columns: TableColumnCtx<T>[];
   data: T[];
 }
 
+/**
+ * 计算表格各列的合计值
+ */
 function getSummary(param: SummaryMethodProps): (string | number)[] {
   const { columns, data } = param;
   const result: (string | number)[] = [];
   columns.forEach((column, index) => {
-    // 第一列为序号，不需要合计
+    // 第一列为序号列，不需要合计
     if (index === 0) {
       result[index] = "合计";
+      return;
+    }
+
+    // 最后一列为操作列，不需要合计
+    if (index === columns.length - 1) {
+      result[index] = "";
       return;
     }
 
@@ -122,18 +186,18 @@ function getSummary(param: SummaryMethodProps): (string | number)[] {
     } else {
       // 其他列则直接提取出对应单元格的值
       values = data.map(
-        (item) => item[column.property as keyof ECardTableEntry]
+        (item) => item[column.property as keyof CardTableEntry]
       );
     }
 
-    // 只有values数组中的所有元素都是有限数字，才将其累加求和
+    // 只有values数组中的所有元素都是有限的数字，才将其累加求和
     if (values.every((value) => Number.isFinite(value))) {
       result[index] = values.reduce((prev, curr) => prev + curr, 0);
     } else {
       ElNotification({
         type: "error",
         title: scriptTitle,
-        message: `表格中存在异常项：${values}`,
+        message: `表格中第${index}列"${column.label}"的待累加数据存在异常项：${values}`,
       });
       result[index] = "";
     }
@@ -141,6 +205,78 @@ function getSummary(param: SummaryMethodProps): (string | number)[] {
   return result;
 }
 
+const loading = ref(false);
+const loadingText = ref("");
+
+/**
+ * 操作E卡
+ */
+async function handleOperation(
+  row: CardTableEntry,
+  operation: number
+): Promise<void> {
+  const { balance, operationNum } = row;
+
+  loading.value = true;
+  loadingText.value = `操作中...0/${operationNum}`;
+
+  const filteredCards = cards.filter((card) => card.balance === balance);
+
+  let count = 0;
+  const operationName = operation === 1 ? "选择" : "反选";
+  // forEach()方法不支持await，因此使用for循环
+  for (const card of filteredCards) {
+    // 需要操作的E卡是status字段的值与传入的operation相等的E卡
+    if (card.status === operation) {
+      // 点击元素
+      card.element.click();
+
+      // 随机睡眠1~2秒
+      const sleepTime = Math.round(Math.random() * 1000 + 1000);
+      await sleep(sleepTime);
+
+      // 更新E卡状态
+      const currentElement = document.querySelector(
+        `div[data-gift-id="${card.id}"]`
+      );
+      card.element = currentElement as HTMLElement;
+      card.status = card.element.className.includes("item-selected") ? 0 : 1;
+
+      if (card.status !== operation) {
+        ElMessage({
+          type: "success",
+          message: `E卡${card.id}（面值${card.balance}）${operationName}成功，睡眠${sleepTime}ms。`,
+        });
+        count++;
+        loadingText.value = `操作中...${count}/${operationNum}`;
+      }
+    }
+
+    if (count === operationNum) {
+      ElMessage({
+        type: "success",
+        message: `面值${balance}的${operationNum}张E卡${operationName}完成！`,
+      });
+      break;
+    }
+  }
+
+  if (count < operationNum) {
+    ElMessage({
+      type: "warning",
+      message: `只有${count}张面值${balance}的E卡可供${operationName}，目前已全部${operationName}完成！`,
+    });
+  }
+
+  countCards();
+
+  loading.value = false;
+  loadingText.value = "";
+}
+
+/**
+ * 从文本值形如“￥123.45”的HTML元素中提取金额
+ */
 function extractNumber(element: Element | null): number {
   if (element && element.textContent !== null) {
     return Number(element.textContent.replace("￥", ""));
@@ -152,6 +288,13 @@ function extractNumber(element: Element | null): number {
     });
     return -1;
   }
+}
+
+/**
+ * 睡眠指定毫秒数
+ */
+function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 onMounted(() => {
